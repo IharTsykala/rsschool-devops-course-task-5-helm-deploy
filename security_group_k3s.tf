3d3d7e86940f3bdc24e79bfdd8e4f0bbda02c4ea
IharTsykala <sudja09@bk.ru>
2024-11-16 14:14:37 +0500

    feat: repository submission]

---
+++ security_group_k3s.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "k3s_security_group" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "Kubernetes API"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP access for WordPress"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "k3s-security-group"
+  }
+}
